fa7153ef938a3fa8c721cd376c260d2010cbccf9
Acea Spades <aceaspades@protonmail.com>
2025-11-18 22:02:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ export default {
       "aceaspades": "308761407319e2c9faefe341a5cfa17060e25320e7c6bb4e33e3f2fecffb1126",
       "coffee": "387e4df5b9d54a2c36754bf91eafe13b3cfbeaf08eaca9f999fb5dae1fad6320",
       "ericfj": "7999814161743f2279006ab10cfd83bd33adff23c85e5d9d0949fdd60f8f1901",
-      "goodmorning": "9d92077c5e35af76f7b1cd84738000b7bafb43d20b0a26c18fe29fa838d27146"
+      "goodmorning": "9d92077c5e35af76f7b1cd84738000b7bafb43d20b0a26c18fe29fa838d27146",
+      "dani": "5bd72972b63ea5d00ae6f7bb88ab2eaf2d9cf0011251b043fffbcc7527d0a1e5",
+      "kevinz": "1d77cae575a8f73ab9e6263913fe78e3a79f40ee25abd72b71ee5d38061c7e4a"
     }
 
     const result = {}
